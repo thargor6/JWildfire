34a328dc477ba50356c0c4ec9d0839be6bbf3b33
Andreas Maschke <thargor6@googlemail.com>
2012-02-12 17:02:38 +0100

    NEW RELEASE 0.38

--- src/org/jwildfire/base/Tools.java
+++ src/org/jwildfire/base/Tools.java
@@ -37,7 +37,7 @@ import org.jwildfire.image.Pixel;
 
 public class Tools {
   public static final String APP_TITLE = "JWildfire";
-  public static final String APP_VERSION = "0.37 (04.02.2012)";
+  public static final String APP_VERSION = "0.38 (12.02.2012)";
 
   public static final double EPSILON = 0.00000001;
   public static final int VPREC = 1024;
