9409e19700cdfe694e161126073bdf2cd10c41af
Andreas Maschke <thargor6@googlemail.com>
2012-05-28 00:26:54 +0200

    Fractal flame editor: new input component for entering numeric values (which is derived from the default spinner component and allows to change values by dragging the mouse)

---
+++ src/org/jwildfire/create/tina/swing/JWFNumberField.java
@@ -0,0 +1,151 @@
+/*
+  JWildfire - an image and animation processor written in Java 
+  Copyright (C) 1995-2012 Andreas Maschke
+
+  This is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser 
+  General Public License as published by the Free Software Foundation; either version 2.1 of the 
+  License, or (at your option) any later version.
+ 
+  This software is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without 
+  even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License along with this software; 
+  if not, write to the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+  02110-1301 USA, or see the FSF site: http://www.fsf.org.
+*/
+package org.jwildfire.create.tina.swing;
+
+import java.awt.Cursor;
+import java.awt.event.MouseEvent;
+
+import javax.swing.JSpinner;
+import javax.swing.SpinnerNumberModel;
+
+import org.jwildfire.base.Tools;
+
+public class JWFNumberField extends JSpinner {
+  private static final double DFLT_STEP = 0.5;
+  private static final double DFLT_MOUSE_THRESHOLD = 10;
+  private static final long serialVersionUID = 1L;
+  private double mouseThreshold;
+  private double valueStep;
+  private boolean hasMinValue = false;
+  private boolean hasMaxValue = false;
+  private double minValue = 0.0;
+  private double maxValue = 0.0;
+  private int xMouseOrigin = 0;
+  private double originValue = 0.0;
+  private SpinnerNumberModel spinnerModel;
+
+  public JWFNumberField() {
+    spinnerModel = new SpinnerNumberModel(new Double(0), null, null, new Double(valueStep));
+    setModel(spinnerModel);
+    setValueStep(DFLT_STEP);
+    setMouseThreshold(DFLT_MOUSE_THRESHOLD);
+    addEvents();
+    setCursor();
+  }
+
+  public double getValueStep() {
+    return valueStep;
+  }
+
+  public void setValueStep(double valueStep) {
+    this.valueStep = valueStep;
+    spinnerModel.setStepSize(valueStep);
+  }
+
+  private void setCursor() {
+    ((JSpinner.DefaultEditor) getEditor()).getTextField().setCursor(Cursor.getPredefinedCursor(Cursor.W_RESIZE_CURSOR));
+  }
+
+  private void addEvents() {
+    ((JSpinner.DefaultEditor) getEditor()).getTextField().addMouseListener(
+        new java.awt.event.MouseAdapter() {
+
+          @Override
+          public void mousePressed(MouseEvent e) {
+            xMouseOrigin = e.getX();
+            originValue = (Double) getValue();
+          }
+
+        });
+
+    ((JSpinner.DefaultEditor) getEditor()).getTextField().addMouseMotionListener(
+        new java.awt.event.MouseMotionAdapter() {
+
+          @Override
+          public void mouseDragged(MouseEvent e) {
+            //            System.out.println("DRAG " + (xMouseOrigin - e.getX()));
+            double THRESHOLD = 10.0;
+            double dx = (xMouseOrigin - e.getX()) / THRESHOLD;
+            double value = (originValue) - dx * valueStep;
+            if (hasMinValue && value < minValue) {
+              value = minValue;
+            }
+            else if (hasMaxValue && value > maxValue) {
+              value = maxValue;
+            }
+            setValue(value);
+          }
+
+        });
+
+  }
+
+  public void setText(String pText) {
+    if (pText != null && pText.length() > 0)
+      setValue(Tools.stringToDouble(pText));
+    else
+      setValue(0.0);
+  }
+
+  public String getText() {
+    return Tools.doubleToString((Double) getValue());
+  }
+
+  public double getMouseThreshold() {
+    return mouseThreshold;
+  }
+
+  public void setMouseThreshold(double mouseThreshold) {
+    this.mouseThreshold = mouseThreshold;
+  }
+
+  public boolean isHasMinValue() {
+    return hasMinValue;
+  }
+
+  public void setHasMinValue(boolean hasMinValue) {
+    this.hasMinValue = hasMinValue;
+    spinnerModel.setMinimum(hasMinValue ? minValue : null);
+  }
+
+  public boolean isHasMaxValue() {
+    return hasMaxValue;
+  }
+
+  public void setHasMaxValue(boolean hasMaxValue) {
+    this.hasMaxValue = hasMaxValue;
+    spinnerModel.setMaximum(hasMaxValue ? maxValue : null);
+  }
+
+  public double getMinValue() {
+    return minValue;
+  }
+
+  public void setMinValue(double minValue) {
+    this.minValue = minValue;
+    spinnerModel.setMinimum(hasMinValue ? minValue : null);
+  }
+
+  public double getMaxValue() {
+    return maxValue;
+  }
+
+  public void setMaxValue(double maxValue) {
+    this.maxValue = maxValue;
+    spinnerModel.setMaximum(hasMaxValue ? maxValue : null);
+  }
+}
