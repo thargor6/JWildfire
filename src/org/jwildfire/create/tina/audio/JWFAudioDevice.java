252735a2e51134b221f6b25a9cc2bc7c6dc8b40e
Andreas Maschke <thargor6@googlemail.com>
2012-11-01 22:07:37 +0100

    Fractal flames editor: Basic jLayer integration to play back music

---
+++ src/org/jwildfire/create/tina/audio/JWFAudioDevice.java
@@ -0,0 +1,118 @@
+/*
+  JWildfire - an image and animation processor written in Java 
+  Copyright (C) 1995-2011 Andreas Maschke
+
+  This is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser 
+  General Public License as published by the Free Software Foundation; either version 2.1 of the 
+  License, or (at your option) any later version.
+ 
+  This software is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without 
+  even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License along with this software; 
+  if not, write to the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+  02110-1301 USA, or see the FSF site: http://www.fsf.org.
+*/
+package org.jwildfire.create.tina.audio;
+
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.Line;
+import javax.sound.sampled.SourceDataLine;
+
+import javazoom.jl.decoder.Decoder;
+import javazoom.jl.decoder.JavaLayerException;
+import javazoom.jl.player.AudioDeviceBase;
+
+public class JWFAudioDevice extends AudioDeviceBase {
+  private final AudioProcessor audioProcessor;
+  private SourceDataLine source = null;
+  private AudioFormat audioFormat = null;
+
+  private int freq;
+  private int channels;
+  private int samplesPerSecond;
+
+  public JWFAudioDevice(AudioProcessor pAudioProcessor) {
+    audioProcessor = pAudioProcessor;
+  }
+
+  protected AudioFormat getAudioFormat() {
+    if (audioFormat == null) {
+      Decoder decoder = getDecoder();
+      audioFormat = new AudioFormat(decoder.getOutputFrequency(),
+          16,
+          decoder.getOutputChannels(),
+          true,
+          false);
+    }
+    return audioFormat;
+  }
+
+  protected DataLine.Info getSourceLineInfo() {
+    AudioFormat fmt = getAudioFormat();
+    DataLine.Info info = new DataLine.Info(SourceDataLine.class, fmt);
+    return info;
+  }
+
+  protected void createSource() throws JavaLayerException {
+    Throwable t = null;
+    try {
+      Line line = AudioSystem.getLine(getSourceLineInfo());
+      if (line instanceof SourceDataLine) {
+        source = (SourceDataLine) line;
+        source.open(audioFormat);
+        source.start();
+      }
+    }
+    catch (Throwable ex) {
+      t = ex;
+    }
+    if (source == null)
+      throw new JavaLayerException("Cannot obtain source audio line", t);
+  }
+
+  protected void closeImpl() {
+    if (source != null) {
+      source.close();
+    }
+  }
+
+  protected void writeImpl(short[] samples, int offs, int len) throws JavaLayerException {
+    if (source == null) {
+      createSource();
+      channels = getDecoder().getOutputChannels();
+      freq = getDecoder().getOutputFrequency();
+      samplesPerSecond = (freq * channels);
+      audioProcessor.init(this);
+    }
+    audioProcessor.process(samples, offs, len);
+  }
+
+  @Override
+  protected void flushImpl() {
+    if (source != null) {
+      source.drain();
+    }
+  }
+
+  public long getFramePosition() {
+    return (source != null) ? source.getLongFramePosition() : 0;
+  }
+
+  public int getSamplesPerSecond() {
+    return samplesPerSecond;
+  }
+
+  public SourceDataLine getSource() {
+    return source;
+  }
+
+  @Override
+  public int getPosition() {
+    return source != null ? source.getFramePosition() : 0;
+  }
+
+}
---
+++ src/org/jwildfire/create/tina/audio/PlayerAudioProcessor.java
@@ -0,0 +1,52 @@
+/*
+  JWildfire - an image and animation processor written in Java 
+  Copyright (C) 1995-2011 Andreas Maschke
+
+  This is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser 
+  General Public License as published by the Free Software Foundation; either version 2.1 of the 
+  License, or (at your option) any later version.
+ 
+  This software is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without 
+  even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License along with this software; 
+  if not, write to the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+  02110-1301 USA, or see the FSF site: http://www.fsf.org.
+*/
+package org.jwildfire.create.tina.audio;
+
+public class PlayerAudioProcessor implements AudioProcessor {
+  private byte[] byteBuf = new byte[4096];
+  private JWFAudioDevice audioDevice;
+
+  @Override
+  public void process(short[] samples, int offs, int len) {
+    byte[] b = toByteArray(samples, offs, len);
+    audioDevice.getSource().write(b, 0, len * 2);
+  }
+
+  @Override
+  public void init(JWFAudioDevice pJWFAudioDevice) {
+    audioDevice = pJWFAudioDevice;
+  }
+
+  private byte[] getByteArray(int length) {
+    if (byteBuf.length < length) {
+      byteBuf = new byte[length + 1024];
+    }
+    return byteBuf;
+  }
+
+  protected byte[] toByteArray(short[] samples, int offs, int len) {
+    byte[] b = getByteArray(len * 2);
+    int idx = 0;
+    short s;
+    while (len-- > 0) {
+      s = samples[offs++];
+      b[idx++] = (byte) s;
+      b[idx++] = (byte) (s >>> 8);
+    }
+    return b;
+  }
+}
