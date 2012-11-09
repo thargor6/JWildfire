40fe3dda787b92bafa948dd76235096b0eb62e2f
Andreas Maschke <thargor6@googlemail.com>
2012-11-09 22:50:53 +0100

    Fractal flames editor: new julian2 variation by Xyrus02

---
+++ src/org/jwildfire/create/tina/variation/JuliaN2Func.java
@@ -0,0 +1,129 @@
+/*
+  JWildfire - an image and animation processor written in Java 
+  Copyright (C) 1995-2011 Andreas Maschke
+
+  This is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser 
+  General Public License as published by the Free Software Foundation; either version 2.1 of the 
+  License, or (at your option) any later version.
+ 
+  This software is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without 
+  even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License along with this software; 
+  if not, write to the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+  02110-1301 USA, or see the FSF site: http://www.fsf.org.
+*/
+package org.jwildfire.create.tina.variation;
+
+import static org.jwildfire.base.MathLib.M_2PI;
+import static org.jwildfire.base.MathLib.atan2;
+import static org.jwildfire.base.MathLib.cos;
+import static org.jwildfire.base.MathLib.iabs;
+import static org.jwildfire.base.MathLib.pow;
+import static org.jwildfire.base.MathLib.sin;
+import static org.jwildfire.base.MathLib.sqr;
+import static org.jwildfire.create.tina.base.Constants.AVAILABILITY_CUDA;
+import static org.jwildfire.create.tina.base.Constants.AVAILABILITY_JWILDFIRE;
+
+import org.jwildfire.base.Tools;
+import org.jwildfire.create.tina.base.XForm;
+import org.jwildfire.create.tina.base.XYZPoint;
+
+public class JuliaN2Func extends VariationFunc {
+  private static final long serialVersionUID = 1L;
+
+  private static final String PARAM_POWER = "power";
+  private static final String PARAM_DIST = "dist";
+  private static final String PARAM_A = "a";
+  private static final String PARAM_B = "b";
+  private static final String PARAM_C = "c";
+  private static final String PARAM_D = "d";
+  private static final String PARAM_E = "e";
+  private static final String PARAM_F = "f";
+  private static final String[] paramNames = { PARAM_POWER, PARAM_DIST, PARAM_A, PARAM_B, PARAM_C, PARAM_D, PARAM_E, PARAM_F };
+
+  private int power = genRandomPower();
+  private double dist = 1.0;
+  private double a = 1.0;
+  private double b = 0.0;
+  private double c = 0.0;
+  private double d = 1.0;
+  private double e = 0.0;
+  private double f = 0.0;
+
+  @Override
+  public void transform(FlameTransformationContext pContext, XForm pXForm, XYZPoint pAffineTP, XYZPoint pVarTP, double pAmount) {
+    double x = a * pAffineTP.x + b * pAffineTP.y + e;
+    double y = c * pAffineTP.x + d * pAffineTP.y + f;
+    double sina = 0.0, cosa = 0.0;
+
+    double angle = (atan2(y, x) + M_2PI * (pContext.random(Integer.MAX_VALUE) % _absN)) / power;
+    double r = pAmount * pow(sqr(x) + sqr(y), _cN);
+
+    sina = sin(angle);
+    cosa = cos(angle);
+    pVarTP.x += r * cosa;
+    pVarTP.y += r * sina;
+    if (pContext.isPreserveZCoordinate()) {
+      pVarTP.z += pAmount * pAffineTP.z;
+    }
+  }
+
+  @Override
+  public String[] getParameterNames() {
+    return paramNames;
+  }
+
+  @Override
+  public Object[] getParameterValues() {
+    return new Object[] { power, dist, a, b, c, d, e, f };
+  }
+
+  @Override
+  public void setParameter(String pName, double pValue) {
+    if (PARAM_POWER.equalsIgnoreCase(pName))
+      power = Tools.FTOI(pValue);
+    else if (PARAM_DIST.equalsIgnoreCase(pName))
+      dist = pValue;
+    else if (PARAM_A.equalsIgnoreCase(pName))
+      a = pValue;
+    else if (PARAM_B.equalsIgnoreCase(pName))
+      b = pValue;
+    else if (PARAM_C.equalsIgnoreCase(pName))
+      c = pValue;
+    else if (PARAM_D.equalsIgnoreCase(pName))
+      d = pValue;
+    else if (PARAM_E.equalsIgnoreCase(pName))
+      e = pValue;
+    else if (PARAM_F.equalsIgnoreCase(pName))
+      f = pValue;
+    else
+      throw new IllegalArgumentException(pName);
+  }
+
+  @Override
+  public String getName() {
+    return "julian2";
+  }
+
+  private int genRandomPower() {
+    int res = (int) (Math.random() * 5.0 + 2.5);
+    return Math.random() < 0.5 ? res : -res;
+  }
+
+  private int _absN;
+  private double _cN;
+
+  @Override
+  public void init(FlameTransformationContext pContext, XForm pXForm, double pAmount) {
+    _absN = iabs(Tools.FTOI(power));
+    _cN = dist / power * 0.5;
+  }
+
+  @Override
+  public int getAvailability() {
+    return AVAILABILITY_JWILDFIRE | AVAILABILITY_CUDA;
+  }
+
+}
