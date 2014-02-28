cf3b8bb083a53cb9321ea5f75437bfda4d21471f
Andreas Maschke <thargor6@googlemail.com>
2014-02-28 23:48:02 +0100

    Refactoring: Reimplemented parts of the blur-shading

--- src/org/jwildfire/create/tina/render/BlurRenderIterationState.java
+++ src/org/jwildfire/create/tina/render/BlurRenderIterationState.java
@@ -8,8 +8,8 @@ import org.jwildfire.create.tina.variation.FlameTransformationContext;
 
 public class BlurRenderIterationState extends DefaultRenderIterationState {
   private static final long serialVersionUID = 1L;
-  private long blurMax;
   private int blurRadius;
+  private double blurFade;
   private double blurKernel[][];
   private int rasterWidth, rasterHeight;
   private RenderColor globalRenderColor = new RenderColor();
@@ -21,24 +21,21 @@ public class BlurRenderIterationState extends DefaultRenderIterationState {
   @Override
   public void init() {
     blurKernel = flame.getShadingInfo().createBlurKernel();
-    //int blurRadius = (int) (flame.getShadingInfo().getBlurRadius() * (flame.getPixelsPerUnit() / 200.0));
     blurRadius = flame.getShadingInfo().getBlurRadius();
 
-    double fade = flame.getShadingInfo().getBlurFade();
-    if (fade < 0.0) {
-      fade = 0.0;
+    blurFade = flame.getShadingInfo().getBlurFade();
+    if (blurFade < 0.0) {
+      blurFade = 0.0;
     }
-    else if (fade > 1.0) {
-      fade = 1.0;
+    else if (blurFade > 1.0) {
+      blurFade = 1.0;
     }
-    blurMax = (long) ((1 - fade) * flame.getSampleDensity() * 100000);
-
     rasterWidth = renderer.rasterWidth;
     rasterHeight = renderer.rasterHeight;
   }
 
   @Override
-  protected void drawPoint(int xIdx, int yIdx, double intensity) {
+  protected void plotPoint(int xIdx, int yIdx, double intensity) {
     RenderColor color;
     if (p.rgbColor) {
       color = globalRenderColor;
@@ -51,7 +48,7 @@ public class BlurRenderIterationState extends DefaultRenderIterationState {
       color = colorMap[colorIdx];
     }
 
-    if (renderThread.getIter() < blurMax) {
+    if (ctx.random() > blurFade) {
       for (int k = yIdx - blurRadius, yk = 0; k <= yIdx + blurRadius; k++, yk++) {
         if (k >= 0 && k < rasterHeight) {
           for (int l = xIdx - blurRadius, xk = 0; l <= xIdx + blurRadius; l++, xk++) {
