db4775079c1d0fbc69e8c3667391737f1e2f7de8
Andreas Maschke <thargor6@googlemail.com>
2014-03-25 23:44:08 +0100

    New "Transforming Duckies" random movie generator

---
+++ src/org/jwildfire/create/tina/randommovie/TransformingDuckiesRandomMovieGenerator.java
@@ -0,0 +1,58 @@
+/*
+  JWildfire - an image and animation processor written in Java 
+  Copyright (C) 1995-2011 Andreas Maschke
+
+  This is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser 
+  General Public License as published by the Free Software Foundation; either version 2.1 of the 
+  License, or (at your option) any later version.
+ 
+  This software is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without 
+  even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License along with this software; 
+  if not, write to the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+  02110-1301 USA, or see the FSF site: http://www.fsf.org.
+*/
+package org.jwildfire.create.tina.randommovie;
+
+import org.jwildfire.base.Prefs;
+import org.jwildfire.create.tina.animate.FlameMovie;
+import org.jwildfire.create.tina.animate.FlameMoviePart;
+import org.jwildfire.create.tina.animate.GlobalScript;
+import org.jwildfire.create.tina.animate.GlobalScriptType;
+import org.jwildfire.create.tina.animate.XFormScript;
+import org.jwildfire.create.tina.animate.XFormScriptType;
+import org.jwildfire.create.tina.base.Flame;
+import org.jwildfire.create.tina.randomflame.DuckiesRandomFlameGenerator;
+
+public class TransformingDuckiesRandomMovieGenerator extends RandomMovieGenerator {
+
+  @Override
+  protected FlameMovie prepareMovie(Prefs pPrefs) {
+    FlameMovie movie = new FlameMovie(pPrefs);
+    Flame flame = genRandomFlame(new DuckiesRandomFlameGenerator(), pPrefs);
+    FlameMoviePart part = new FlameMoviePart();
+    part.setFlame(flame);
+    part.setFrameCount(320);
+    part.setFrameMorphCount(0);
+    movie.addPart(part);
+    {
+      double amplitude = -1.5 * Math.random() * 3.0;
+      movie.setGlobalScript1(new GlobalScript(GlobalScriptType.ROTATE_ROLL, amplitude));
+    }
+
+    {
+      double amplitude = -0.5 * Math.random() * 1.0;
+      movie.setxFormScript1(new XFormScript(XFormScriptType.ROTATE_2ND_XFORM, amplitude));
+    }
+
+    return movie;
+  }
+
+  @Override
+  public String getName() {
+    return "Transforming Duckies";
+  }
+
+}
