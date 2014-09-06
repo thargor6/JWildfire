7bf248c0f114ab9c3db943d4d3d92d78a9a52e4a
Andreas Maschke <thargor6@googlemail.com>
2014-09-06 11:45:14 +0200

    new "dinis_surface"-variation

---
+++ src/org/jwildfire/create/tina/variation/DinisSurfaceWFFunc.java
@@ -0,0 +1,71 @@
+/*
+  JWildfire - an image and animation processor written in Java 
+  Copyright (C) 1995-2011 Andreas Maschke
+
+  This is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser 
+  General Public License as published by the Free Software Foundation; either version 2.1 of the 
+  License, or (at your option) any later version.
+ 
+  This software is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without 
+  even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License along with this software; 
+  if not, write to the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+  02110-1301 USA, or see the FSF site: http://www.fsf.org.
+*/
+package org.jwildfire.create.tina.variation;
+
+import static org.jwildfire.base.mathlib.MathLib.cos;
+import static org.jwildfire.base.mathlib.MathLib.log;
+import static org.jwildfire.base.mathlib.MathLib.sin;
+
+import org.jwildfire.create.tina.base.XForm;
+import org.jwildfire.create.tina.base.XYZPoint;
+
+public class DinisSurfaceWFFunc extends VariationFunc {
+  private static final long serialVersionUID = 1L;
+
+  private static final String PARAM_A = "a";
+  private static final String PARAM_B = "b";
+  private static final String[] paramNames = { PARAM_A, PARAM_B };
+
+  private double a = 0.8;
+  private double b = 0.2;
+
+  @Override
+  public void transform(FlameTransformationContext pContext, XForm pXForm, XYZPoint pAffineTP, XYZPoint pVarTP, double pAmount) {
+    /* Dini's Surface, http://mathworld.wolfram.com/DinisSurface.html  */
+    double u = pAffineTP.x;
+    double v = pAffineTP.y;
+    double sinv = sin(v);
+    pVarTP.x += pAmount * a * cos(u) * sinv;
+    pVarTP.y += pAmount * a * sin(u) * sinv;
+    pVarTP.z -= pAmount * (a * (cos(v) + log(v / 2.0)) + b * u);
+  }
+
+  @Override
+  public String[] getParameterNames() {
+    return paramNames;
+  }
+
+  @Override
+  public Object[] getParameterValues() {
+    return new Object[] { a, b };
+  }
+
+  @Override
+  public void setParameter(String pName, double pValue) {
+    if (PARAM_A.equalsIgnoreCase(pName))
+      a = pValue;
+    else if (PARAM_B.equalsIgnoreCase(pName))
+      b = pValue;
+    else
+      throw new IllegalArgumentException(pName);
+  }
+
+  @Override
+  public String getName() {
+    return "dinis_surface_wf";
+  }
+}
