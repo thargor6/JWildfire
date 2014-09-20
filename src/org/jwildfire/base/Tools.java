6aea05fb7f0cecb4e6690ea83a7dea6dc0f97cf1
Andreas Maschke <thargor6@googlemail.com>
2014-09-20 22:50:22 +0200

    New 2.0 ALPHA 3 release

--- src/org/jwildfire/base/Tools.java
+++ src/org/jwildfire/base/Tools.java
@@ -45,7 +45,7 @@ import org.jwildfire.image.Pixel;
 
 public class Tools {
   public static final String APP_TITLE = "JWildfire";
-  public static final String APP_VERSION = "2.00 ALPHA 2 (14.09.2014)";
+  public static final String APP_VERSION = "2.00 ALPHA 3 (20.09.2014)";
 
   public static boolean SPECIAL_VERSION = false;
 
