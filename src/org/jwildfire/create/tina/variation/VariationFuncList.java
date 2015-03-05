c171ed84e7567cb976570931151fbe76c151926d
Gregg Helt <gregghelt@gmail.com>
2015-03-04 19:47:21 -0800

    Registering variation Mask with VariationFuncList

--- src/org/jwildfire/create/tina/variation/VariationFuncList.java
+++ src/org/jwildfire/create/tina/variation/VariationFuncList.java
@@ -426,6 +426,7 @@ public class VariationFuncList {
     registerVariationFunc(DCBubbleFunc.class);
     registerVariationFunc(DCTriangleFunc.class);
     registerVariationFunc(WaveBlurWFFunc.class);
+    registerVariationFunc(MaskFunc.class);
   }
 
   private static void registerVariationFunc(
