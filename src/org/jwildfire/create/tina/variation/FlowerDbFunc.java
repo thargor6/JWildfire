cab081218000406726dd21cfd8360af2e3d3e09c
Gregg Helt <gregghelt@gmail.com>
2015-03-29 06:02:39 -0700

    Enhanced equations to allow for adjusting of stem thickness, stem length, adding a fold to the petals and adjusting location and effect of fold. Removed "thickness" parameter and added "petal_split" parameter, which has a similar effect but the effect is now relative to the petal count ("petal" param), so that for integer petal count and petal split, will split each petal into petal_split sub-petals.

--- src/org/jwildfire/create/tina/variation/FlowerDbFunc.java
+++ src/org/jwildfire/create/tina/variation/FlowerDbFunc.java
@@ -9,32 +9,57 @@ import static org.jwildfire.base.mathlib.MathLib.sin;
 import static org.jwildfire.base.mathlib.MathLib.cos;
 import static org.jwildfire.base.mathlib.MathLib.fabs;
 
-
 /*
  *  Original author dark-beam
  *  code snippets relayed by Don Town
- *  transcribed and turned into full variation by CozyG
+ *  transcribed, extended, and turned into full variation by CozyG
+ *  
+ *  Currently only behaves if no translation in pre-transform?
  */
 public class FlowerDbFunc extends VariationFunc {
   private static final long serialVersionUID = 1L;
 
   private static final String PARAM_PETALS = "petals";
-  private static final String PARAM_THICKNESS = "thickness";
-  private static final String PARAM_SPREAD = "spread";
+  private static final String PARAM_PETAL_SPLIT = "petal_split";
+  private static final String PARAM_PETAL_SPREAD = "petal_spread";
+  private static final String PARAM_STEM_THICKNESS = "stem_thickness";
+  private static final String PARAM_STEM_LENGTH = "stem_length";
+  private static final String PARAM_PETAL_FOLD_STRENGTH = "petal_fold_strength";
+  private static final String PARAM_PETAL_FOLD_RADIUS = "petal_fold_radius";
   
-  private static final String[] paramNames = { PARAM_PETALS, PARAM_THICKNESS, PARAM_SPREAD };
+  private static final String[] paramNames = { PARAM_PETALS, PARAM_PETAL_SPLIT, PARAM_PETAL_SPREAD, PARAM_STEM_THICKNESS, PARAM_STEM_LENGTH,
+                                               PARAM_PETAL_FOLD_STRENGTH, PARAM_PETAL_FOLD_RADIUS };
 
-  private double petals = 8;
-  private double thickness = 1;
-  private double spread = 1;
+  // non-integer petals is possible, changes relative size of petals
+  private double petals = 6;
+  // non-integer petal_splits are possible, changes relative size of petals
+  private double petal_split = 0;
+  private double petal_spread = 1;
+  private double stem_thickness = 1;
+  // default stem_length of 0 is considered special case,
+  // if stem_length = 0, then stem length doesn't truncate, so extends "infinitely" while fading away
+  private double stem_length = 0;
+  // how strong (added angle) the fold is, positive value fold up, negative values fold down
+  private double petal_fold_strength = 0;
+  // how far out 
+  private double petal_fold_radius= 1;
 
   public void transform(FlameTransformationContext pContext, XForm pXForm, XYZPoint pAffineTP, XYZPoint pVarTP, double pAmount) {
-       double r =  pAmount *sqrt(pAffineTP.getPrecalcSumsq());
-       double t = pAffineTP.getPrecalcAtanYX();
-       r = r * (fabs ( (spread + sin(petals*t)) * cos(thickness*t) ) );
-       pVarTP.x += sin(t) * r;
-       pVarTP.y += cos(t) * r;
-       pVarTP.z += (2.0 / r - 1.0);
+    double r =  pAmount * sqrt(pAffineTP.getPrecalcSumsq());
+    double t = pAffineTP.getPrecalcAtanYX();
+    r = r * (fabs ( (petal_spread + sin(petals * t)) * cos(petal_split * petals * t) ) );
+       
+    pVarTP.x += sin(t) * r;
+    pVarTP.y += cos(t) * r;
+    pVarTP.z -= stem_thickness * ((2.0 / r) - 1.0);
+    double rnew = sqrt((pVarTP.x * pVarTP.x) + (pVarTP.y * pVarTP.y));
+    if (rnew > petal_fold_radius) {
+      pVarTP.z +=  (rnew - petal_fold_radius) * petal_fold_strength;
+    }
+    if ((stem_length != 0) && (pVarTP.z <= (-1 * stem_length))) {
+      pVarTP.z = (-1 * stem_length);
+    }
+          
   }
 
   @Override
@@ -49,17 +74,25 @@ public class FlowerDbFunc extends VariationFunc {
 
   @Override
   public Object[] getParameterValues() {
-    return new Object[] { petals, thickness, spread };
+    return new Object[] { petals, petal_split, petal_spread, stem_thickness, stem_length, petal_fold_strength, petal_fold_radius };
   }
 
   @Override
   public void setParameter(String pName, double pValue) {
     if (PARAM_PETALS.equalsIgnoreCase(pName))
       petals = pValue;
-    else if (PARAM_THICKNESS.equalsIgnoreCase(pName))
-      thickness = pValue;
-    else if (PARAM_SPREAD.equalsIgnoreCase(pName))
-      spread = pValue;
+    else if (PARAM_PETAL_SPLIT.equalsIgnoreCase(pName))
+      petal_split = pValue;
+    else if (PARAM_PETAL_SPREAD.equalsIgnoreCase(pName))
+      petal_spread = pValue;
+    else if (PARAM_STEM_THICKNESS.equalsIgnoreCase(pName))
+      stem_thickness = pValue;
+    else if (PARAM_STEM_LENGTH.equalsIgnoreCase(pName))
+      stem_length = pValue;
+    else if (PARAM_PETAL_FOLD_STRENGTH.equalsIgnoreCase(pName))
+      petal_fold_strength = pValue;
+    else if (PARAM_PETAL_FOLD_RADIUS.equalsIgnoreCase(pName))
+      petal_fold_radius = pValue;
     else
       throw new IllegalArgumentException(pName);
   }
