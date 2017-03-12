707a6315944410f0a97c256b26f14118d791c273
Andreas Maschke <Andreas Maschke>
2017-03-12 11:07:31 +0100

    NEW RELEASE 3.02

--- src/org/jwildfire/base/Tools.java
+++ src/org/jwildfire/base/Tools.java
@@ -56,7 +56,7 @@ import org.jwildfire.image.Pixel;
 
 public class Tools {
   public static final String APP_TITLE = "JWildfire";
-  public static final String APP_VERSION = "3.01 (07.01.2017)";
+  public static final String APP_VERSION = "3.02 (12.03.2017)";
 
   public static final boolean SPECIAL_VERSION = false;
 
