486e1a7ff18c1dbd5fdbe9a4aae153c82fe8f942
Rick Sidwell <richard.d.sidwell@gmail.com>
2017-11-25 20:20:32 -0700

    New variations: crop3D, spherecrop, and loq
    crop3D and spherecrop are 3D versions of crop and circlecrop.
    loq is a quaternion variation like sinq and cosq that was missed when
    the others were added.

---
+++ src/org/jwildfire/create/tina/variation/LoqFunc.java
@@ -0,0 +1,74 @@
+/*
+  JWildfire - an image and animation processor written in Java 
+  Copyright (C) 1995-2011 Andreas Maschke
+
+  This is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser 
+  General Public License as published by the Free Software Foundation; either version 2.1 of the 
+  License, or (at your option) any later version.
+ 
+  This software is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without 
+  even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License along with this software; 
+  if not, write to the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+  02110-1301 USA, or see the FSF site: http://www.fsf.org.
+*/
+package org.jwildfire.create.tina.variation;
+
+import static org.jwildfire.base.mathlib.MathLib.*;
+
+import org.jwildfire.create.tina.base.Layer;
+import org.jwildfire.create.tina.base.XForm;
+import org.jwildfire.create.tina.base.XYZPoint;
+
+public class LoqFunc extends VariationFunc {
+  private static final long serialVersionUID = 1L;
+
+  private static final String PARAM_BASE = "base";
+  
+  private static final String[] paramNames = { PARAM_BASE };
+  
+  private double base = M_E;
+  private double denom;
+
+  @Override
+  public void init(FlameTransformationContext pContext, Layer pLayer, XForm pXForm, double pAmount) {
+	  denom = 0.5 * pAmount / log(base);
+  }
+  
+  @Override
+  public void transform(FlameTransformationContext pContext, XForm pXForm, XYZPoint pAffineTP, XYZPoint pVarTP, double pAmount) {
+    /* Loq by zephyrtronium http://zephyrtronium.deviantart.com/art/Quaternion-Apo-Plugin-Pack-165451482 */
+
+	double abs_v = Math.hypot (pAffineTP.y,pAffineTP.z);
+    double C = pAmount * atan2(abs_v, pAffineTP.x) / abs_v;
+    pVarTP.x += log(sqr(pAffineTP.x) + sqr(abs_v)) * denom;
+    pVarTP.y += C * pAffineTP.y;
+    pVarTP.z += C * pAffineTP.z; 
+  }
+
+  @Override
+  public String[] getParameterNames() {
+    return paramNames;
+  }
+
+  @Override
+  public Object[] getParameterValues() {
+    return new Object[] { base };
+  }
+
+  @Override
+  public void setParameter(String pName, double pValue) {
+    if (PARAM_BASE.equalsIgnoreCase(pName))
+      base = pValue;
+    else
+        throw new IllegalArgumentException(pName);
+    }
+
+  @Override
+  public String getName() {
+    return "loq";
+  }
+
+}
