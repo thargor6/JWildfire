15104964c6e6f6738d9be3b5d129d8f1735bd507
thargor6 <thargor6@googlemail.com>
2019-06-19 23:51:52 +0200

    optimized the settings for random batches

---
+++ src/org/jwildfire/create/tina/base/weightingfield/WeightingFieldVariationBackList.java
@@ -0,0 +1,4 @@
+package org.jwildfire.create.tina.base.weightingfield;
+
+public class WeightingFieldVariationBackList {
+}
--- src/org/jwildfire/create/tina/swing/RandomBatchQuality.java
+++ src/org/jwildfire/create/tina/swing/RandomBatchQuality.java
@@ -1,3 +1,19 @@
+/*
+  JWildfire - an image and animation processor written in Java
+  Copyright (C) 1995-2019 Andreas Maschke
+
+  This is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser
+  General Public License as published by the Free Software Foundation; either version 2.1 of the
+  License, or (at your option) any later version.
+
+  This software is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+  even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License along with this software;
+  if not, write to the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+  02110-1301 USA, or see the FSF site: http://www.fsf.org.
+*/
 package org.jwildfire.create.tina.swing;
 
 public enum RandomBatchQuality {
@@ -9,29 +25,29 @@ public enum RandomBatchQuality {
 
     @Override
     public int getMaxSamples() {
-      return 10;
+      return 8;
     }
   },
   NORMAL {
     @Override
     public double getCoverage() {
-      return 0.48;
+      return 0.42;
     }
 
     @Override
     public int getMaxSamples() {
-      return 32;
+      return 16;
     }
   },
   HIGH {
     @Override
     public double getCoverage() {
-      return 0.64;
+      return 0.56;
     }
 
     @Override
     public int getMaxSamples() {
-      return 48;
+      return 24;
     }
   };
 
