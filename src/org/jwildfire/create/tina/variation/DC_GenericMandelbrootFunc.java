a9729010b760b6443e66446f83f62259b6369376
thargor6 <thargor6@googlemail.com>
2021-04-03 13:25:37 +0200

    included Rick's notes to the manual

--- src/org/jwildfire/create/tina/variation/DC_GenericMandelbrootFunc.java
+++ src/org/jwildfire/create/tina/variation/DC_GenericMandelbrootFunc.java
@@ -395,7 +395,7 @@ public class DC_GenericMandelbrootFunc  extends DC_BaseFunc {
 	
 	@Override
 	public VariationFuncType[] getVariationTypes() {
-		return new VariationFuncType[]{VariationFuncType.VARTYPE_2D, VariationFuncType.VARTYPE_SIMULATION, VariationFuncType.VARTYPE_DC, VariationFuncType.VARTYPE_BASE_SHAPE};
+		return new VariationFuncType[]{VariationFuncType.VARTYPE_2D, VariationFuncType.VARTYPE_SIMULATION, VariationFuncType.VARTYPE_DC, VariationFuncType.VARTYPE_BASE_SHAPE, VariationFuncType.VARTYPE_ESCAPE_TIME_FRACTAL};
 	}
 	
 }
