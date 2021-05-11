9f6a2bd5fa6573cb95c419884bae6b1df7d35065
thargor6 <thargor6@googlemail.com>
2021-05-11 02:17:32 +0200

    added GPU-support for some more variations

--- src/org/jwildfire/create/tina/variation/Splits3DFunc.java
+++ src/org/jwildfire/create/tina/variation/Splits3DFunc.java
@@ -1,6 +1,6 @@
 /*
   JWildfire - an image and animation processor written in Java 
-  Copyright (C) 1995-2011 Andreas Maschke
+  Copyright (C) 1995-2021 Andreas Maschke
 
   This is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser 
   General Public License as published by the Free Software Foundation; either version 2.1 of the 
@@ -19,7 +19,7 @@ package org.jwildfire.create.tina.variation;
 import org.jwildfire.create.tina.base.XForm;
 import org.jwildfire.create.tina.base.XYZPoint;
 
-public class Splits3DFunc extends VariationFunc {
+public class Splits3DFunc extends VariationFunc implements SupportsGPU {
   private static final long serialVersionUID = 1L;
 
   private static final String PARAM_X = "x";
@@ -83,7 +83,25 @@ public class Splits3DFunc extends VariationFunc {
 
   @Override
   public VariationFuncType[] getVariationTypes() {
-    return new VariationFuncType[]{VariationFuncType.VARTYPE_3D};
+    return new VariationFuncType[]{VariationFuncType.VARTYPE_3D, VariationFuncType.VARTYPE_SUPPORTS_GPU};
   }
 
+  @Override
+  public String getGPUCode(FlameTransformationContext context) {
+    // based on code from the cudaLibrary.xml compilation, created by Steven Brodhead Sr.
+    return "if(__x >= 0.0)\n"
+        + "    __px += varpar->splits3D * (__x + varpar->splits3D_x);\n"
+        + "else\n"
+        + "    __px += varpar->splits3D * (__x - varpar->splits3D_x);\n"
+        + "\n"
+        + "if(__y >= 0.0)\n"
+        + "    __py += varpar->splits3D * (__y + varpar->splits3D_y);\n"
+        + "else\n"
+        + "    __py += varpar->splits3D * (__y - varpar->splits3D_y);\n"
+        + "\n"
+        + "if(__z >= 0.0)\n"
+        + "    __pz += varpar->splits3D * (__z + varpar->splits3D_z);\n"
+        + "else\n"
+        + "    __pz += varpar->splits3D * (__z - varpar->splits3D_z);";
+  }
 }
