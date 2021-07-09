2b4455c7570f3213e542dbce072a9ed415fd0659
snicker02 <snicker02@gmail.com>
2021-07-08 21:37:33 -0400

    Update YPlot3DWFFunc.java

--- src/org/jwildfire/create/tina/variation/plot/YPlot3DWFFunc.java
+++ src/org/jwildfire/create/tina/variation/plot/YPlot3DWFFunc.java
@@ -508,6 +508,7 @@ public class YPlot3DWFFunc extends VariationFunc implements SupportsGPU {
       VariationFuncType.VARTYPE_BASE_SHAPE,
       VariationFuncType.VARTYPE_DC,
       VariationFuncType.VARTYPE_EDIT_FORMULA
+      VariationFuncType.VARTYPE_SUPPORTS_GPU  
     };
   }
 
