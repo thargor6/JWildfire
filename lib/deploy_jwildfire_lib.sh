507488d97c61a48513e39b7a1747d9f3a3f094cb
thargor6 <thargor6@googlemail.com>
2022-01-22 22:35:26 +0100

    some fixes to the build file

--- lib/deploy_jwildfire_lib.sh
+++ lib/deploy_jwildfire_lib.sh
@@ -1,2 +1,2 @@
-export REPO_PATH=file:../.jwildfire-lib/.m2/repository/
-mvn deploy:deploy-file -Durl=$REPO_PATH -Dfile=../target/jwildfire-lib-7.31-SNAPSHOT-shaded.jar -DgroupId=jwildfire.org -DartifactId=jwildfire-lib -Dpackaging=jar -Dversion=7.40.0-SNAPSHOT
+export REPO_PATH=file:../../JWildfireLib/orig_repo/.m2/repository/
+mvn deploy:deploy-file -Durl=$REPO_PATH -Dfile=../target/jwildfire-lib-7.40-SNAPSHOT-shaded.jar -DgroupId=org.jwildfire -DartifactId=jwildfire-lib -Dpackaging=jar -Dversion=7.40.0-SNAPSHOT
