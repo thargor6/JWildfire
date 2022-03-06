eaa08064b762443ccfcf21d6b68331e522e4aa7d
Andreas Maschke <thargor6@googlemail.com>
2022-03-06 13:30:37 +0100

    added new variationtype "Supported by JWildfire Swan"

--- src/org/jwildfire/create/tina/variation/Swirl3Func.java
+++ src/org/jwildfire/create/tina/variation/Swirl3Func.java
@@ -68,7 +68,7 @@ public class Swirl3Func extends VariationFunc implements SupportsGPU {
 
   @Override
   public VariationFuncType[] getVariationTypes() {
-    return new VariationFuncType[]{VariationFuncType.VARTYPE_2D, VariationFuncType.VARTYPE_SUPPORTS_GPU};
+    return new VariationFuncType[]{VariationFuncType.VARTYPE_2D, VariationFuncType.VARTYPE_SUPPORTS_GPU, VariationFuncType.VARTYPE_SUPPORTED_BY_SWAN};
   }
 
   @Override
