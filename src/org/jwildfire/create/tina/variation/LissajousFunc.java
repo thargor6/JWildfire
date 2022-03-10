ea457fedc51f0aa36b09f2a10b0b75a4c3c62f2c
Andreas Maschke <thargor6@googlemail.com>
2022-03-10 22:30:12 +0100

    added new variationtype "Supported by JWildfire Swan"

--- src/org/jwildfire/create/tina/variation/LissajousFunc.java
+++ src/org/jwildfire/create/tina/variation/LissajousFunc.java
@@ -97,7 +97,7 @@ public class LissajousFunc extends VariationFunc implements SupportsGPU {
 
   @Override
   public VariationFuncType[] getVariationTypes() {
-    return new VariationFuncType[]{VariationFuncType.VARTYPE_2D, VariationFuncType.VARTYPE_SUPPORTS_GPU};
+    return new VariationFuncType[]{VariationFuncType.VARTYPE_2D, VariationFuncType.VARTYPE_SUPPORTS_GPU, VariationFuncType.VARTYPE_SUPPORTED_BY_SWAN};
   }
 
   @Override
