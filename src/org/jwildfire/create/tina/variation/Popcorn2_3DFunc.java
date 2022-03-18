63d836294cbe2942355738db50a5a4b19fda9131
Andreas Maschke <thargor6@googlemail.com>
2022-03-18 23:33:03 +0100

    added new variationtype "Supported by JWildfire Swan"

--- src/org/jwildfire/create/tina/variation/Popcorn2_3DFunc.java
+++ src/org/jwildfire/create/tina/variation/Popcorn2_3DFunc.java
@@ -100,7 +100,7 @@ public class Popcorn2_3DFunc extends VariationFunc implements SupportsGPU {
 
   @Override
   public VariationFuncType[] getVariationTypes() {
-    return new VariationFuncType[]{VariationFuncType.VARTYPE_3D, VariationFuncType.VARTYPE_SUPPORTS_GPU};
+    return new VariationFuncType[]{VariationFuncType.VARTYPE_3D, VariationFuncType.VARTYPE_SUPPORTS_GPU, VariationFuncType.VARTYPE_SUPPORTED_BY_SWAN};
   }
 
   @Override
