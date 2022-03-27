c6f2e2fe804fbee6d21537f30be007cc6eeae1d4
Andreas Maschke <thargor6@googlemail.com>
2022-03-27 23:48:33 +0200

    added new variationtype "Supported by JWildfire Swan"

--- src/org/jwildfire/create/tina/variation/XHeartFunc.java
+++ src/org/jwildfire/create/tina/variation/XHeartFunc.java
@@ -92,7 +92,7 @@ public class XHeartFunc extends VariationFunc implements SupportsGPU {
 
   @Override
   public VariationFuncType[] getVariationTypes() {
-    return new VariationFuncType[]{VariationFuncType.VARTYPE_2D, VariationFuncType.VARTYPE_SUPPORTS_GPU};
+    return new VariationFuncType[]{VariationFuncType.VARTYPE_2D, VariationFuncType.VARTYPE_SUPPORTS_GPU, VariationFuncType.VARTYPE_SUPPORTED_BY_SWAN};
   }
 
   @Override
