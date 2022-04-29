ec2cb001faf09cfadce8ae319d616a29bf19561c
Andreas Maschke <thargor6@googlemail.com>
2022-04-30 00:31:57 +0200

    added new variationtype "Supported by JWildfire Swan"

--- src/org/jwildfire/create/tina/variation/IntersectionFunc.java
+++ src/org/jwildfire/create/tina/variation/IntersectionFunc.java
@@ -145,7 +145,7 @@ public class IntersectionFunc extends VariationFunc implements SupportsGPU {
 
   @Override
   public VariationFuncType[] getVariationTypes() {
-    return new VariationFuncType[]{VariationFuncType.VARTYPE_2D,VariationFuncType.VARTYPE_SUPPORTS_GPU};
+    return new VariationFuncType[]{VariationFuncType.VARTYPE_2D,VariationFuncType.VARTYPE_SUPPORTS_GPU, VariationFuncType.VARTYPE_SUPPORTED_BY_SWAN};
   }
   @Override
   public String getGPUCode(FlameTransformationContext context) {
