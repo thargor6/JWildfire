09405ab7bd665e8a4cc4929d49396fc12093f297
Andreas Maschke <thargor6@googlemail.com>
2022-05-30 23:23:23 +0200

    added new variationtype "Supported by JWildfire Swan"

--- src/org/jwildfire/create/tina/variation/ExtrudeFunc.java
+++ src/org/jwildfire/create/tina/variation/ExtrudeFunc.java
@@ -66,7 +66,7 @@ public class ExtrudeFunc extends VariationFunc implements SupportsGPU {
 
   @Override
   public VariationFuncType[] getVariationTypes() {
-    return new VariationFuncType[]{VariationFuncType.VARTYPE_ZTRANSFORM, VariationFuncType.VARTYPE_SUPPORTS_GPU};
+    return new VariationFuncType[]{VariationFuncType.VARTYPE_ZTRANSFORM, VariationFuncType.VARTYPE_SUPPORTS_GPU, VariationFuncType.VARTYPE_SUPPORTED_BY_SWAN};
   }
 
   @Override
--- src/org/jwildfire/create/tina/variation/PreZTranslateFunc.java
+++ src/org/jwildfire/create/tina/variation/PreZTranslateFunc.java
@@ -39,7 +39,7 @@ public class PreZTranslateFunc extends SimpleVariationFunc implements SupportsGP
 
   @Override
   public VariationFuncType[] getVariationTypes() {
-    return new VariationFuncType[]{VariationFuncType.VARTYPE_ZTRANSFORM, VariationFuncType.VARTYPE_PRE, VariationFuncType.VARTYPE_SUPPORTS_GPU};
+    return new VariationFuncType[]{VariationFuncType.VARTYPE_ZTRANSFORM, VariationFuncType.VARTYPE_PRE, VariationFuncType.VARTYPE_SUPPORTS_GPU, VariationFuncType.VARTYPE_SUPPORTED_BY_SWAN};
   }
 
   @Override
