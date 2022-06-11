91d1a72872848e9a19378ddbe42b56827e2647d7
Andreas Maschke <thargor6@googlemail.com>
2022-06-11 18:12:26 +0200

    added new variationtype "Supported by JWildfire Swan"

--- src/org/jwildfire/create/tina/variation/FlowerDbFunc.java
+++ src/org/jwildfire/create/tina/variation/FlowerDbFunc.java
@@ -117,7 +117,7 @@ public class FlowerDbFunc extends VariationFunc implements SupportsGPU {
 
   @Override
   public VariationFuncType[] getVariationTypes() {
-    return new VariationFuncType[]{VariationFuncType.VARTYPE_2D, VariationFuncType.VARTYPE_SUPPORTS_GPU};
+    return new VariationFuncType[]{VariationFuncType.VARTYPE_2D, VariationFuncType.VARTYPE_SUPPORTS_GPU, VariationFuncType.VARTYPE_SUPPORTED_BY_SWAN};
   }
 
   @Override
--- src/org/jwildfire/create/tina/variation/GlynniaFunc.java
+++ src/org/jwildfire/create/tina/variation/GlynniaFunc.java
@@ -88,7 +88,7 @@ public class GlynniaFunc extends SimpleVariationFunc implements SupportsGPU {
 
   @Override
   public VariationFuncType[] getVariationTypes() {
-    return new VariationFuncType[]{VariationFuncType.VARTYPE_2D, VariationFuncType.VARTYPE_SUPPORTS_GPU};
+    return new VariationFuncType[]{VariationFuncType.VARTYPE_2D, VariationFuncType.VARTYPE_SUPPORTS_GPU, VariationFuncType.VARTYPE_SUPPORTED_BY_SWAN};
   }
 
   @Override
