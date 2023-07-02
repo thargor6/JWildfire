6682840b49241ef45c6098c6131d0d1d5191faa3
thargor6 <thargor6@googlemail.com>
2023-07-02 07:59:18 +0200

    -upgraded the WIX- and jpackage-versions, so this is only a technical update

--- lib/deploy_jwildfire_lib.sh
+++ lib/deploy_jwildfire_lib.sh
@@ -1,2 +1,2 @@
 export REPO_PATH=file:../../JWildfireLib/orig_repo/.m2/repository/
-mvn deploy:deploy-file -Durl=$REPO_PATH -Dfile=../target/jwildfire-lib-7.43-SNAPSHOT-shaded.jar -DgroupId=org.jwildfire -DartifactId=jwildfire-lib -Dpackaging=jar -Dversion=7.43.0-SNAPSHOT
+mvn deploy:deploy-file -Durl=$REPO_PATH -Dfile=../target/jwildfire-lib-7.55-SNAPSHOT-shaded.jar -DgroupId=org.jwildfire -DartifactId=jwildfire-lib -Dpackaging=jar -Dversion=7.55.0-SNAPSHOT
--- src/org/jwildfire/base/Tools.java
+++ src/org/jwildfire/base/Tools.java
@@ -61,9 +61,9 @@ import org.jwildfire.io.ImageReader;
 
 public class Tools {
   public static final String APP_TITLE = "JWildfire";
-  private static final String APP_VERSION = "V8.10 (21.03.2023)";
+  private static final String APP_VERSION = "V8.11 (02.07.2023)";
 
-  public static final boolean STEAM_EDITION = false;
+  public static final boolean STEAM_EDITION = true;
   public static final boolean SPECIAL_VERSION = false;
 
   public static final int MAX_SPATIAL_OVERSAMPLING = 6;
