2ea5284a7c81581658595083a87fd82cd3c6d565
thargor6 <thargor6@googlemail.com>
2023-12-09 05:50:04 +0100

    the "shift"-parameter of the gradient can now be easilier be animate to created color-cycling animations (as suggested by Hugo Orosz)

--- src/org/jwildfire/create/tina/palette/RGBPalette.java
+++ src/org/jwildfire/create/tina/palette/RGBPalette.java
@@ -1,6 +1,6 @@
 /*
   JWildfire - an image and animation processor written in Java 
-  Copyright (C) 1995-2020 Andreas Maschke
+  Copyright (C) 1995-2023 Andreas Maschke
 
   This is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser 
   General Public License as published by the Free Software Foundation; either version 2.1 of the 
@@ -265,7 +265,7 @@ public class RGBPalette implements Assignable<RGBPalette>, Serializable {
       }
       for (int i = 0; i < PALETTE_SIZE; i++) {
         RGBColor color = getRawColor(i);
-        int idx = i + modShift;
+        int idx = i + modShift % 255;
         if (idx < 0) {
           idx += PALETTE_SIZE;
         }
