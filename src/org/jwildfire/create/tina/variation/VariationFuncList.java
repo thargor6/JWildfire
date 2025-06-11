faa92746bb218ea222c594832b52176fcced6607
snicker02 <snicker02@gmail.com>
2025-06-11 14:51:47 -0400

    Update VariationFuncList.java

--- src/org/jwildfire/create/tina/variation/VariationFuncList.java
+++ src/org/jwildfire/create/tina/variation/VariationFuncList.java
@@ -1049,6 +1049,7 @@ VariationFuncList {
     registerVariationFunc(GeometricPrimitivesFunc.class);
     registerVariationFunc(CactusGlobeFunc.class);
     registerVariationFunc(JuliaScopePlusFunc.class);
+    registerVariationFunc(RomanescoFunc.class);
 
     
 
