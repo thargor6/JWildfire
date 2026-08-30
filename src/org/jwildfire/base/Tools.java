2a26806eb7487ea60eb3b1a554c8a73120d54343
thargor6 <thargor6@googlemail.com>
2026-08-31 00:35:03 +0200

    RELEASE 9.20

--- src/org/jwildfire/base/Tools.java
+++ src/org/jwildfire/base/Tools.java
@@ -61,7 +61,7 @@ import org.jwildfire.io.ImageReader;
 
 public class Tools {
   public static final String APP_TITLE = "JWildfire";
-  private static final String APP_VERSION = "V9.10 (14.01.2026)";
+  private static final String APP_VERSION = "V9.20 (30.08.2026)";
 
   public static final boolean STEAM_EDITION = false;
   public static final boolean SPECIAL_VERSION = false;
